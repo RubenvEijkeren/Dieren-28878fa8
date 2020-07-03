c8ab46c98f563238beada566ae7da3f6cb971e78
RubenvEijkeren <54809240+RubenvEijkeren@users.noreply.github.com>
2020-07-03 11:45:19 +0200

    Uitwerking van de exercise

---
+++ Vis.class.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'Dier.class.php';
+
+class Vis extends Dier
+{
+    public function __construct()
+    {
+        $this->dier = 'Vis';
+    }
+
+    public function speak() {
+        echo "blub blub" . PHP_EOL;
+    }
+}
+?>
---
+++ Vogel.class.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'Dier.class.php';
+
+class Vogel extends Dier
+{
+    public function __construct()
+    {
+        $this->dier = 'Vogel';
+    }
+
+    public function speak() {
+        echo "chirp chirp" . PHP_EOL;
+    }
+}
+?>
